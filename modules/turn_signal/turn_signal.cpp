86a4ed373e002b3a5c0b561c40fb5a810c161bfe
bechtele <bechtele@union.edu>
2024-03-04 23:06:55 +0000

    first change

--- modules/turn_signal/turn_signal.cpp
+++ modules/turn_signal/turn_signal.cpp
@@ -13,10 +13,10 @@
 //=====[Declaration of private data types]=====================================
 
 typedef enum {
-    DECIDE,
-    WAIT,
-    BUTTON_PRESSED,
-    BUTTON_RELEASED
+    BUTTON_UP,
+    BUTTON_DOWN,
+    BUTTON_FALLING,
+    BUTTON_RISING
 } buttonState_t;
 
 //=====[Declaration and initialization of public global objects]===============
@@ -24,7 +24,7 @@ typedef enum {
 DigitalIn leftTurnsignal(PD_14); //Make sure these are acceptable GPIO ports
 DigitalIn rightTurnsignal(PE_12);
 
-buttonState_t buttonState = DECIDE;
+buttonState_t buttonState = BUTTON_UP;
 //buttonState_t buttonStateRight = BUTTON_RELEASED;
 
 bool outLeft = false;
@@ -50,109 +50,3 @@ bool readLeftTurnsignal(){ //Replace with FSM to debounce
 
 //VERSION 02
 
-static bool debounceInput()
-{
-    static int timeElap = 0;
-    static bool buttonTracker;
-
-    switch( buttonState ) {
-
-    case BUTTON_RELEASED:
-        if( leftTurnsignal ) {
-            buttonState = WAIT;
-        }
-        break;
-
-    case BUTTON_PRESSED:
-        if (timeElap > DEBOUNCE_BUTTON_TIME_MS) {
-            timeElap=0;
-            if (buttonTracker == LEFT && leftTurnsignal) {
-                buttonState =
-            }
-            else if (buttonTracker == RIGHT && rightTurnsignal) {
-                return 2;
-            }
-            else{
-
-            }
-
-        }
-        
-        
-        break;
-
-    case WAIT:
-            if( leftTurnsignal || rightTurnsignal ) { 
-                buttonTracker = leftTurnsignal;
-                buttonState = BUTTON_PRESSED;
-            } 
-            else {
-                buttonState = WAIT;
-            }   
-    break; 
-
-    case WAIT_RELEASE : //fix this shit; what if glitch happen after press; controller think button go up 
-        if(!leftTurnsignal){
-            buttonState = BUTTON_RELEASED;
-        }
-    break;  
-        
-    
-    return false;
-    }
-}
-
-//VERSION 03
-
-static bool debounceInput()
-{
-    static int timeElap = 0;
-    static bool buttonTracker;
-
-    switch( buttonState ) {
-
-    case DECIDE:
-        if(leftTurnsignal){
-            buttonTracker = LEFT;
-            buttonState = WAIT;
-        }
-        if(rightTurnsignal){
-            buttonTracker = RIGHT;
-            buttonState = WAIT;
-        }
-
-
-        break;
-
-    case WAIT:
-        if (timeElap > DEBOUNCE_BUTTON_TIME_MS) {
-            timeElap=0;
-            if (buttonTracker == LEFT && leftTurnsignal || buttonTracker == RIGHT && rightTurnsignal) {
-                buttonState = BUTTON_PRESSED;
-            }
-            else{
-                buttonState = DECIDE;
-            }
-        }
-        
-        break;
-
-    case BUTTON_PRESSED:
-            if( buttonTracker ) { 
-                outLeft = !outLeft;
-                outRight = false;
-            } 
-            if( !buttonTracker ) {
-                outLeft = false;
-                outRight = !outRight;
-            }
-
-        if (buttonTracker == LEFT && !leftTurnsignal || buttonTracker == RIGHT && !rightTurnsignal) {
-            buttonState = WAIT;
-        }
-
-    break;    
-    
-    return false;
-    }
-}
